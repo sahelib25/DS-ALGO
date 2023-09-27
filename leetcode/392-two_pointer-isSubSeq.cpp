8a820b5d24f2e260f451a79f056f5851b0f43c84
Saheli Bhattacharjee <47847054+sahelib25@users.noreply.github.com>
2023-09-27 18:11:26 +0100

    Add 392

---
+++ leetcode/392-two_pointer-isSubSeq.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    // two pointer approach
+    bool isSubsequence(string s, string t) {
+        int m = s.size();
+        int n = t.size();
+        int i = 0 , j = 0;
+        while(i<m && j<n){
+            if (s[i] == t[j]){
+                i++;
+            }
+            j++;
+        }
+        return i == m ? 1 : 0;
+    }
+
+
+};
