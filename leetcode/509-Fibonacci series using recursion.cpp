05b6d1be1222a9ff7817806ef35fe6f8f99addf6
Saheli Bhattacharjee <47847054+sahelib25@users.noreply.github.com>
2023-09-28 06:54:17 +0100

    Update

--- leetcode/509-Fibonacci series using recursion.cpp
+++ leetcode/509-Fibonacci series using recursion.cpp
@@ -7,3 +7,24 @@ public:
         return fib(n-1) + fib(n-2);
     }
 };
+
+// Using Dynamic programming
+// class Solution {
+
+// public: 
+//     int calc(vector<int>& dp, int n){
+//         if (n<=1){
+//             return n;
+//         }
+//         if(dp[n] != -1){
+//             return dp[n];
+//         }
+//         return calc(dp, n-1) + calc(dp, n-2);
+//     }
+// public:
+//     int fib(int n) {
+//         vector<int> dp(n+1, -1);
+        
+//         return calc(dp,n);
+//     }
+// };
