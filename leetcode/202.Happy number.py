15c9bb8a715c821395ba6caa102bd81178c7d9c6
Saheli Bhattacharjee <47847054+sahelib25@users.noreply.github.com>
2025-05-21 20:47:49 +0100

    Create 202.Happy number.py

---
+++ leetcode/202.Happy number.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        
+        visit = set()
+
+        while n!=1:
+            if n in visit:
+                return False
+
+            visit.add(n)
+
+            num_to_str = str(n)
+            ans = 0
+            for i in num_to_str:
+                digit = int(i)
+                ans += digit ** 2
+
+            n = ans
+            
+        return True # When n becomes 1
